1e7f09def0006e94d071923d0aa7144c2db0750a
Antônio Rocha <antoniojunior159@gmail.com>
2022-05-28 16:23:46 -0300

    Create main

--- main.py
+++ main.py
@@ -1,35 +1,45 @@
-#Consumindo uma API com Python
-import requests 
+import requests
+import PySimpleGUI as sg
 
-def apresentacao():
-	print("===================")
-	print("===CUNSULTA CEP ===")
-	print("===================")
+#Criando o tema
+sg.theme("DarkTeal12")
+def index():	
+	layout = [
+		[sg.Text("Digite um CEP: ", font=" Helvetica 20")],
+		[sg.Input(key = '-CEP-', size=20, font=" Any 16")],
+		[sg.Button("Consultar", font="16"), sg.Button("Nova Consulta", font="16"), sg.Button("Sair", font="16")],
+		[sg.Text("")],
+		[sg.Text("logradouro     ", font="16"), sg.Input(size=(50), font=" Any 16", key="-OUT1-")],
+		[sg.Text("")],
+		[sg.Text("Complemento", font="16"), sg.Input(size=(50), font=" Any 16", key="-OUT2-")],
+		[sg.Text("")],
+		[sg.Text("Bairro             ", font="16"), sg.Input(size=(50), font=" Any 16", key="-OUT3-")],
+		[sg.Text("")],
+		[sg.Text("Cidade           ", font="16"), sg.Input(size=(50), font=" Any 16", key="-OUT4-")],
+		[sg.Text("")],
+		[sg.Text("Estado            ", font="16"), sg.Input(size=(50), font=" Any 16", key="-OUT5-")]
+	
+]
+	window = sg.Window('Sistema de Consultar CEP', layout, size=(500,600), element_justification='center'  )
 
-	cep = input("Digite o CEP: ")
-
-	if len(cep) != 8:
-		print("Quantidade de Dígitos Inválido!")
-		print()
-		apresentacao()
-#Criando requsição
-	r = requests.get("https://viacep.com.br/ws/{}/json/".format(cep))
-
-	endereco = r.json()
-	if 'erro' not in endereco:
-		print("CEP Encontrado:")
-		print()
-		print("CEP: {}".format(endereco["cep"]))
-		print("Logradouro: {}".format(endereco["logradouro"]))
-		print("Complemento: {}".format(endereco["complemento"]))
-		print("Bairro: {}".format(endereco["bairro"]))
-		print("Cidade: {}".format(endereco["localidade"]))
-		print("Estado: {}".format(endereco["uf"]))
-		print()
-		apresentacao()
-	else:
-		print("CEP Inválido!")
-		print()
-		apresentacao()
-
-apresentacao()
+	while True:
+		event, values = window.read()
+		if event == sg.WINDOW_CLOSED or event == 'Sair':
+			break
+		elif event == 'Consultar':
+			cep = values['-CEP-']
+			r = requests.get("https://viacep.com.br/ws/{}/json/".format(cep))
+			endereco = r.json()
+			window['-OUT1-'].update(endereco["logradouro"])
+			window['-OUT2-'].update(endereco["complemento"])
+			window['-OUT3-'].update(endereco["bairro"])
+			window['-OUT4-'].update(endereco["localidade"])
+			window['-OUT5-'].update(endereco["uf"])
+		elif event == "Nova Consulta":
+			window['-CEP-'].update("")
+			window['-OUT1-'].update("")
+			window['-OUT2-'].update("")
+			window['-OUT3-'].update("")
+			window['-OUT4-'].update("")
+			window['-OUT5-'].update("")
+index()
